91eb3c915a69d095fa9ccb1332d0306465e60861
Dario George <edu.dariogeorge21@gmail.com>
2025-09-28 16:48:38 +0530

    Remove TypeScript and ESLint build configurations
    Removed TypeScript and ESLint configurations to enforce stricter build checks.

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,6 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-      },
 };
 
 module.exports = nextConfig;
